0cce7c9e376266c4c790a46fb37c872c5209c910
HakimChe <144377630+HakimChe@users.noreply.github.com>
2024-03-13 18:08:02 +0100

    Add files via upload

--- interrogerCRAB.sql
+++ interrogerCRAB.sql
@@ -12,7 +12,10 @@ inner join Bornes on Bornes.idBorne = Interventions.idBorne where Interventions.
 select idInterv, Bornes.code, Technicien.nomTech, Technicien.prenomTech from Interventions inner join Technicien on Interventions.idTech = Technicien.idTech
 inner join Bornes on Bornes.idBorne = Interventions.idBorne where Interventions.idBorne != 1 ;
 
+select * from Interventions where dateHeureFin is NULL ;
+
 select * from Interventions where timediff(DateHeureDebut, dateHeureFin) < '-03:00:00.000000' ;
 
-select *, count(dateHeureFin) from Interventions group by idBorne ;
+select *, count(idBorne) from Interventions group by idBorne ;
 
+select *, count(idBorne) from Interventions group by idBorne having count(idBorne) > 2 ;
